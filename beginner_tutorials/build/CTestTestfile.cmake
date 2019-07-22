f8933babfd173e4277ba1113a2595066ac15c8e6
Your Name <you@example.com>
2019-07-22 19:00:09 +0200

    lab 1 - removed build

--- beginner_tutorials/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user_en/catkin_ws/src/beginner_tutorials
-# Build directory: /home/user_en/catkin_ws/src/beginner_tutorials/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
